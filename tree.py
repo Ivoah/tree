c642c85b891555f30b5e20d596da699ca62dbdc2
Noah Rosamilia <ivoahivoah@gmail.com>
2017-03-17 00:15:08 -0400

    Add initial option support

--- tree.py
+++ tree.py
@@ -40,37 +40,48 @@ def colorize(path, full = False):
 
     return file
 
-def print_dir(dir, pre = ''):
+def print_dir(dir, pre = '', opts = {}):
     dirs = 0
     files = 0
+    size = 0
 
     if pre == '': print(colors.dir + dir + colors.end)
 
     dir_len = len(os.listdir(dir)) - 1
     for i, file in enumerate(sorted(os.listdir(dir), key = str.lower)):
         path = os.path.join(dir, file)
-        if file[0] == '.': continue
-        print(pre + strs[2 if i == dir_len else 1] + colorize(path))
+        if file[0] == '.' and not opts['show_hidden']: continue
         if os.path.isdir(path):
+            print(pre + strs[2 if i == dir_len else 1] + colorize(path))
             if os.path.islink(path):
                 dirs += 1
             else:
-                d, f = print_dir(path, pre + strs[3 if i == dir_len else 0])
+                d, f, s = print_dir(path, pre + strs[3 if i == dir_len else 0], opts = opts)
                 dirs += d + 1
                 files += f
+                size += s
         else:
             files += 1
+            size += os.path.getsize(path)
+            print(pre + strs[2 if i == dir_len else 1] + ('[{:>11}]  '.format(size) if opts['show_size'] else '') + colorize(path))
 
-    return (dirs, files)
+
+    return (dirs, files, size)
 
 dirs = 0
 files = 0
 
+opts = {
+    'show_hidden': False,
+    'show_size': False,
+    'follow_symlinks': False
+}
+
 if len(sys.argv) == 1:
-    dirs, files = print_dir('.')
+    dirs, files, size = print_dir('.', opts = opts)
 else:
     for dir in sys.argv[1:]:
-        d, f = print_dir(dir)
+        d, f = print_dir(dir, opts = opts)
         dirs += d
         files += f
 
